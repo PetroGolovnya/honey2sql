252b6fc3565d2553e59e768cd08b7b23c63330a2
Alexandr <amaliuk6@gmail.com>
2018-03-16 22:46:06 +0700

    Create hohey2sql.h

---
+++ hohey2sql.h
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "src/netapi_types.h"
+#include "src/netapi_user.h"
+
+extern int point_numbers(char*, int, POINT_NUMBER_DATA_2*);
+extern int param_numbers(char*, int, PARAM_NUMBER_DATA_2*);
+extern int get_hist_values(char*, int, rgethstpar_date_data_2*);
+extern void dispaly_usage();
